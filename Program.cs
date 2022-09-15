bdce433c5653f337bfc3fa548a928b9f324c24f6
Nothung429 <frank.gatto96.fg@gmail.com>
2022-09-15 15:44:38 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -3,18 +3,18 @@
 string[] foods = { "riso patate e cozze", "pizza", "hamburger", "sushi", "pokè", "piadina", "involtini", "pollo"};
 
 int lengthOfFoods = foods.Length;
-
+Console.WriteLine("Lunghezza lista");
 Console.WriteLine(lengthOfFoods);
 
-for(var i = 0; i < lengthOfFoods; i++)
+for (var i = 0; i < lengthOfFoods; i++)
 {
     Console.WriteLine(foods[i]);
 };
 
 string favourite = foods.First();
-
+Console.WriteLine("Cibo preferito");
 Console.WriteLine(favourite);
 
 string hated = foods.Last();
-
+Console.WriteLine("Cibo odiato");
 Console.WriteLine(hated);
