6bba214ba265f143698f365ffbca42d27eea79ca
Nothung429 <frank.gatto96.fg@gmail.com>
2022-09-15 16:12:53 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] foods = { "riso patate e cozze", "pizza", "hamburger", "sushi", "pokè", "piadina", "involtini", "pollo"/*, "cipolla"*/};
+string[] foods = { "riso patate e cozze", "pizza", "hamburger", "sushi", "pokè", "piadina", "involtini", "pollo", "cipolla"};
 
 int lengthOfFoods = foods.Length;
-Console.WriteLine("Lunghezza lista:");
-Console.WriteLine(lengthOfFoods);
+Console.WriteLine($"Lunghezza lista: {lengthOfFoods}");
 
 for (var i = 0; i < lengthOfFoods; i++)
 {
@@ -12,13 +11,11 @@ for (var i = 0; i < lengthOfFoods; i++)
 };
 
 string favourite = foods.First();
-Console.WriteLine("Cibo preferito:");
-Console.WriteLine(favourite);
+Console.WriteLine($"Cibo preferito: {favourite}");
 
 string hated = foods.Last();
-Console.WriteLine("Cibo odiato:");
-Console.WriteLine(hated);
+Console.WriteLine($"Cibo odiato: {hated}");
 
 int im = lengthOfFoods / 2;
 string mediana = foods[im];
-Console.WriteLine(mediana);
+Console.WriteLine($"Cibo mediano: {mediana}");
