76ee0e0680403d0fa3528dd030b7aae5a1f62833
Nothung429 <frank.gatto96.fg@gmail.com>
2022-09-15 15:27:52 +0200

    add pricipal solutions to exercise

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] foods = { "riso patate e cozze", "pizza", "hamburger", "sushi", "pokè", "piadina", "involtini", "pollo"};
+
+int lengthOfFoods = foods.Length;
+
+Console.WriteLine(lengthOfFoods);
+
+for(var i = 0; i < lengthOfFoods; i++)
+{
+    Console.WriteLine(foods[i]);
+};
+
+string favourite = foods.First();
+
+Console.WriteLine(favourite);
+
+string hated = foods.Last();
+
+Console.WriteLine(hated);
